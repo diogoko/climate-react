d689bc6bc011c0d367f0c3829ec903af499195a9
Diogo Kollross <diogoko@gmail.com>
2021-03-31 23:40:53 -0300

    Setup basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Card, CardContent, CardHeader, Container, makeStyles, Typography } from '@material-ui/core';
+import RefreshIcon from '@material-ui/icons/Refresh';
+
+const useStyles = makeStyles((theme) => ({
+  temperature: {
+    alignItems: 'baseline',
+    display: 'flex',
+    justifyContent: 'center',
+    marginBottom: theme.spacing(2),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline/>
+
+      <Container maxWidth="sm" component="main">
+        <Card>
+          <CardHeader
+            action={<RefreshIcon/>}
+            subheader="Brazil"
+            subheaderTypographyProps={{ align: 'center' }}
+            title="São José do Rio Preto"
+            titleTypographyProps={{ align: 'center' }}
+          />
+
+          <CardContent>
+            <div className={classes.temperature}>
+              <Typography component="h2" variant="h3" color="textPrimary">
+                22
+              </Typography>
+              <Typography variant="h6" color="textSecondary">
+                ℃
+              </Typography>
+            </div>
+
+            <ul>
+              <Typography component="li" variant="subtitle1" align="left">
+                Clear
+              </Typography>
+              <Typography component="li" variant="subtitle1" align="left">
+                Feels like 21℃
+              </Typography>
+              <Typography component="li" variant="subtitle1" align="left">
+                Minimum 18℃
+              </Typography>
+              <Typography component="li" variant="subtitle1" align="left">
+                Maximum 32℃
+              </Typography>
+              <Typography component="li" variant="subtitle1" align="left">
+                Humidity 44%
+              </Typography>
+            </ul>
+          </CardContent>
+        </Card>
+      </Container>
+    </React.Fragment>
   );
 }
 
