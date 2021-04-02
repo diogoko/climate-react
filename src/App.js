52ec15fb9e506e4b26ef3aa9051e66c9fd0d2d23
Diogo Kollross <diogoko@gmail.com>
2021-04-01 23:25:31 -0300

    Extract SearchInput component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { AppBar, Card, CardContent, CardHeader, Container, fade, IconButton, InputBase, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Card, CardContent, CardHeader, Container, IconButton, makeStyles, Toolbar, Typography } from '@material-ui/core';
 import RefreshIcon from '@material-ui/icons/Refresh';
-import SearchIcon from '@material-ui/icons/Search';
 import GpsFixedIcon from '@material-ui/icons/GpsFixed';
+import SearchInput from './SearchInput';
 
 const useStyles = makeStyles((theme) => ({
   temperature: {
@@ -11,45 +11,6 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     justifyContent: 'center',
     marginBottom: theme.spacing(2),
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
   },
   main: {
     marginTop: '1em',
@@ -70,19 +31,7 @@ function App() {
               <GpsFixedIcon/>
             </IconButton>
 
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon/>
-              </div>
-              <InputBase
-                placeholder="Search…"
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-                inputProps={{ 'aria-label': 'search' }}
-              />
-            </div>
+            <SearchInput/>
           </Toolbar>
         </AppBar>
 
