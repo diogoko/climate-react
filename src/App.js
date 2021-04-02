b7442f072593902eddba9e95d19e85ef1f960c17
Diogo Kollross <diogoko@gmail.com>
2021-04-01 23:00:23 -0300

    Complete basic layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { Card, CardContent, CardHeader, Container, makeStyles, Typography } from '@material-ui/core';
+import { AppBar, Card, CardContent, CardHeader, Container, fade, IconButton, InputBase, makeStyles, Toolbar, Typography } from '@material-ui/core';
 import RefreshIcon from '@material-ui/icons/Refresh';
+import SearchIcon from '@material-ui/icons/Search';
+import GpsFixedIcon from '@material-ui/icons/GpsFixed';
 
 const useStyles = makeStyles((theme) => ({
   temperature: {
@@ -10,6 +12,48 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'center',
     marginBottom: theme.spacing(2),
   },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+  main: {
+    marginTop: '1em',
+  }
 }));
 
 function App() {
@@ -19,7 +63,29 @@ function App() {
     <React.Fragment>
       <CssBaseline/>
 
-      <Container maxWidth="sm" component="main">
+      <Container maxWidth="sm" component="main" className={classes.main}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton color="inherit">
+              <GpsFixedIcon/>
+            </IconButton>
+
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon/>
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+
         <Card>
           <CardHeader
             action={<RefreshIcon/>}
